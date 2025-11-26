5020785c748c5c6b8fe05514ca7ac8f5dec5ea23
preethisuresh23 <preethipooja2319@gmail.com>
2025-11-26 22:23:00 +0530

    Fix PORT and MongoDB variable

--- server.js
+++ server.js
@@ -23,5 +23,6 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/resources", require("./routes/resourceRoutes"));
 
 // start server
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
