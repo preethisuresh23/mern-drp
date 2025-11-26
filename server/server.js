90b15c7f2bc1240863916509697b6082f2052f92
preethisuresh23 <preethipooja2319@gmail.com>
2025-11-26 23:09:01 +0530

    Fix port for Railway

--- server/server.js
+++ server/server.js
@@ -23,5 +23,7 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/resources", require("./routes/resourceRoutes"));
 
 // start server
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
